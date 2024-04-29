89f9042b69819e946f87d723a3b304f37d8a850a
thokjend <thokjend@online.no>
2024-04-29 13:36:25 +0200

    *

---
+++ Fra JS til C#/FirstCsProgram/MyConsole.cs
@@ -0,0 +1,23 @@
+﻿namespace FirstCsProgram
+{
+    internal class MyConsole
+    {
+        public static string Ask(string question)
+        {
+            Console.Write(question + " ");
+            return Console.ReadLine();
+        }
+        public static int AskForInt(string question)
+        {
+            var answerStr = Ask(question); 
+            return Convert.ToInt32(answerStr);
+        }
+
+        public static bool AskForBool(string question)
+        {
+            var answerStr = Ask(question + " (y/n) ");
+            return answerStr.ToLower().StartsWith("y");
+        }
+
+    }
+}
---
+++ Fra JS til C#/FirstCsProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstCsProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = MyConsole.Ask("What is your name?");
+            Console.WriteLine($"Hello, {name}");
+
+            var birthYear = MyConsole.AskForInt("What year were you born?");
+            var hadBirthday = MyConsole.AskForBool("Have you had birthday this year?");
+            var age = 2024 - birthYear;
+            if (!hadBirthday)
+            {
+                age--;
+            }
+            Console.WriteLine($"You are {age} years old");
+        }
+    }
+}
