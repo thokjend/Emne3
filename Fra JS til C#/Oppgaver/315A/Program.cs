c790e25d2b58818dec11116970811d5f386a48e1
thokjend <thokjend@online.no>
2024-04-29 14:38:40 +0200

    *

--- Fra JS til C#/Oppgaver/315A/Program.cs
+++ Fra JS til C#/Oppgaver/315A/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var number = random.Next(1, 100);
+
+            Console.WriteLine("Guess a number between 1 and 100");
+            string guessStr = Console.ReadLine();
+            int guess = int.Parse(guessStr);
+            int guessCount = 0;
+
+            if (guess < number)
+            {
+                Console.WriteLine($"Too low, you guessed {guess} with a total of {guessCount} guesses. Try again!");
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine($"Too high, you guessed {guess} with a total of {guessCount} guesses. Try again!");
+            }
+            else
+            {
+                Console.WriteLine("You Won!");
+            }
+
         }
     }
 }
--- Fra JS til C#/Oppgaver/315A/obj/Debug/net8.0/315A.AssemblyInfo.cs
+++ Fra JS til C#/Oppgaver/315A/obj/Debug/net8.0/315A.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("315A")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0bf69443a8bee412be4242b4a04cd93f193ade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465df4d7dec95368c1b00809669288e0d373baf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("315A")]
 [assembly: System.Reflection.AssemblyTitleAttribute("315A")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
