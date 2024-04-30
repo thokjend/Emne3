6be73c24527cda92dc2d1c02b7f24578799ca695
thokjend <thokjend@online.no>
2024-04-30 10:51:53 +0200

    Update Program.cs

--- Fra JS til C#/Oppgaver/315A/Program.cs
+++ Fra JS til C#/Oppgaver/315A/Program.cs
@@ -14,6 +14,7 @@
 
             while (number != guess)
             {
+                guessCount++;
                 if (guess < number)
                 {
                     Console.WriteLine($"Too low, you guessed {guess} with a total of {guessCount} guesses. Try again!");
