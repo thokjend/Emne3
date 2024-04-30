9fc8dbcbbca1d702c30cdc45b5125f200c712ec5
thokjend <thokjend@online.no>
2024-04-30 12:41:57 +0200

    Update Program.cs

--- Fra JS til C#/Oppgaver/315B/Program.cs
+++ Fra JS til C#/Oppgaver/315B/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Funksjon som skal telle antall bokstaver i en tekst
+            int countLetter = 0;
+            Console.Write("Enter letter: ");
+            string letterStr = Console.ReadLine().ToLower();
+            char letter = letterStr.ToCharArray()[0];
+
+            while (letterStr.Length != 1)
+            {
+                Console.Write("Please enter a single letter: ");
+                letterStr = Console.ReadLine().ToLower();
+                letter = letterStr.ToCharArray()[0];
+            }
+            Console.Write("Enter text: ");
+
+            string text = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == letter)
+                {
+                    countLetter++;
+                }
+            }
+
+            Console.WriteLine(countLetter);
         }
     }
 }
