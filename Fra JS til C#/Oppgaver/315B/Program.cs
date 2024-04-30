11651538f71f621911db279d128f044aef759ad2
thokjend <thokjend@online.no>
2024-04-30 12:59:29 +0200

    Update Program.cs

--- Fra JS til C#/Oppgaver/315B/Program.cs
+++ Fra JS til C#/Oppgaver/315B/Program.cs
@@ -8,28 +8,28 @@
             int countLetter = 0;
             Console.Write("Enter letter: ");
             string letterStr = Console.ReadLine().ToLower();
-            //char letter = letterStr.ToCharArray()[0];
+            char letter = letterStr.ToCharArray()[0];
 
             while (letterStr.Length != 1)
             {
                 Console.Write("Please enter a single letter: ");
                 letterStr = Console.ReadLine().ToLower();
-                //letter = letterStr.ToCharArray()[0];
+                letter = letterStr.ToCharArray()[0];
             }
             Console.Write("Enter text: ");
 
             string text = Console.ReadLine().ToLower();
 
-            for (int i = 0; i < text.Length; i++)
+
+            foreach (char c in text)
             {
-                //if (text[i] == letter)
-                if (text[i] == letterStr[0])
+                if (c == letter)
                 {
                     countLetter++;
                 }
             }
 
-            Console.WriteLine(countLetter);
+            Console.WriteLine($"{letter}: {countLetter}");
         }
     }
 }
