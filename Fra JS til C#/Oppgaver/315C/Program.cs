b5846de1d0d486d9ec603138dd6604269d1f8fb3
thokjend <thokjend@online.no>
2024-05-02 12:53:56 +0200

    Update Program.cs

--- Fra JS til C#/Oppgaver/315C/Program.cs
+++ Fra JS til C#/Oppgaver/315C/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace _315C
+﻿using System.Globalization;
+
+namespace _315C
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = Console.ReadLine();
+            string reversedText = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversedText += text[i];
+            }
+            Console.WriteLine(reversedText);
         }
     }
 }
