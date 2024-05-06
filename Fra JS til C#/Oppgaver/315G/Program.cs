a8b9b0659983c5fc7e490dec7f6163abc6f3143d
thokjend <thokjend@online.no>
2024-05-06 13:39:13 +0200

    *

---
+++ Fra JS til C#/Oppgaver/315G/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] arguments)
+    {
+        var range = 250;
+        var counts = new int[range];
+        string text = "something";
+        int counter = 0;
+        while (!string.IsNullOrWhiteSpace(text))
+        {
+            text = Console.ReadLine();
+            foreach (var character in text)
+            {
+                counts[(int)character]++;
+                counter++;
+            }
+            for (var i = 0; i < range; i++)
+            {
+                if (counts[i] > 0)
+                {
+                    var character = (char)i;
+                    float percentage = (float)counts[i] / counter * 100;
+                    Console.WriteLine($"{character} -  {counts[i]} - {percentage:F2}%");
+                }
+            }
+        }
+    }
+}
