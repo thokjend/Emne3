2ff3f08619189677e9551688ed77f78a79ea3655
thokjend <thokjend@online.no>
2024-05-06 12:42:12 +0200

    *

---
+++ Fra JS til C#/Oppgaver/315E/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _315E
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] getNumbers = numbers(1000, 1028, 7);
+
+            for (int i = 0; i < getNumbers.Length; i++)
+            {
+                Console.WriteLine(getNumbers[i]);
+            }
+
+        }
+
+        static int[] numbers(int start, int end, int diff)
+        {
+            int indexes = (end - start) / diff + 1;
+            int[] numbers = new int[indexes];
+            int currentIndex = 0;
+
+            for (int i = start; i <= end; i += diff)
+            {
+                numbers[currentIndex] = i;
+                currentIndex++;
+            }
+
+            return numbers;
+        }
+
+    }
+}
