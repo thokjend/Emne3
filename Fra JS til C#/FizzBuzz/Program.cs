52fc019f5804caac2ea22d3002324c1f82a42bc3
thokjend <thokjend@online.no>
2024-05-10 12:33:02 +0200

    *

---
+++ Fra JS til C#/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                return;
+            }
+
+            int amount = int.Parse(args[0]);
+
+            for (int i = 1; i <= amount; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
