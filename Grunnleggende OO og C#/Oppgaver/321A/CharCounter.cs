e85e382bd1d8b4f50f2c5f1f61bd22d0f52bf3d5
thokjend <thokjend@online.no>
2024-05-13 13:12:27 +0200

    *

---
+++ Grunnleggende OO og C#/Oppgaver/321A/CharCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _321A
+{
+    internal class CharCounter
+    {
+        public int[] counts = new int[250];
+
+        public void AddText(string text)
+        {
+            foreach (var character in text ?? String.Empty)
+            {
+                counts[(int)character]++;
+            }
+        }
+
+        public void ShowCounts()
+        {
+            for (var i = 0; i < counts.Length; i++)
+            {
+                if (counts[i] > 0)
+                {
+                    var character = (char)i;
+                    Console.WriteLine($"{character} -  {counts[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ Grunnleggende OO og C#/Oppgaver/321A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+
+namespace _321A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new CharCounter();
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine();
+                counter.AddText(text);
+                counter.ShowCounts();
+            }
+        }
+    }
+}
