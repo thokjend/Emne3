56651201de72cf60e916f6a055ce1dc0d7cee471
thokjend <thokjend@online.no>
2024-05-14 15:16:37 +0200

    *

---
+++ Grunnleggende OO og C#/Oppgaver/321B/Model.cs
@@ -0,0 +1,58 @@
+﻿namespace _321B
+{
+    internal class Model
+    {
+        public points Points;
+        public rows Rows;
+        public data Data;
+    }
+
+    class points
+    {
+        public int CodeBreaker;
+        public int CodeMaker;
+    }
+
+    class rows
+    {
+        public string[] SecretRow;
+        public guessRows GuessRows;
+        public feedbackRows FeedbackRows;
+
+    }
+
+    class guessRows
+    {
+        public string[] row1;
+        public string[] row2;
+        public string[] row3;
+        public string[] row4;
+        public string[] row5;
+        public string[] row6;
+        public string[] row7;
+        public string[] row8;
+        public string[] row9;
+        public string[] row10;
+    }
+
+    class feedbackRows
+    {
+        public string[] row1;
+        public string[] row2;
+        public string[] row3;
+        public string[] row4;
+        public string[] row5;
+        public string[] row6;
+        public string[] row7;
+        public string[] row8;
+        public string[] row9;
+        public string[] row10;
+    }
+
+    class data
+    {
+        public int SelectedIndex;
+        public bool GoToNextRound;
+    }
+
+}
---
+++ Grunnleggende OO og C#/Oppgaver/321B/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+
+namespace _321B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var model = new Model
+            {
+                Points = new points()
+                {
+                    CodeBreaker = 10,
+                    CodeMaker = 8,
+                },
+                Rows = new rows()
+                {
+                    GuessRows = new guessRows()
+                    {
+                        row1 = ["white","red","blue","green"],
+                        row2 = ["white", "red", "blue", "green"],
+                        row3 = ["white", "red", "blue", "green"],
+                        row4 = ["white", "red", "blue", "green"],
+                        row5 = ["white", "red", "blue", "green"],
+                        row6 = ["white", "red", "blue", "green"],
+                        row7 = ["white", "red", "blue", "green"],
+                        row8 = ["white", "red", "blue", "green"],
+                        row9 = ["white", "red", "blue", "green"],
+                        row10 = ["white", "red", "blue", "green"],
+                    },
+                    FeedbackRows = new feedbackRows()
+                    {
+                        row1 = ["white", "red", "red", "red"],
+                        row2 = ["white", "red", "red", "red"],
+                        row3 = ["white", "red", "red", "red"],
+                        row4 = ["white", "red", "red", "red"],
+                        row5 = ["white", "red", "red", "red"],
+                        row6 = ["white", "red", "red", "red"],
+                        row7 = ["white", "red", "red", "red"],
+                        row8 = ["white", "red", "red", "red"],
+                        row9 = ["white", "red", "red", "red"],
+                        row10 = ["white", "red", "red", "red"],
+                    },
+                    SecretRow = ["white", "red", "blue", "green"]
+
+                },
+                Data = new data()
+                {
+                    SelectedIndex = 3,
+                    GoToNextRound = false,
+                }
+            };
+
+
+            Console.WriteLine(model.Rows.SecretRow[2]);
+        }
+        
+    }
+}
