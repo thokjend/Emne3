900cbefa659be82755d6d84c305c0ce0ad5acd58
thokjend <thokjend@online.no>
2024-05-15 14:15:21 +0200

    *

--- Grunnleggende OO og C#/Oppgaver/323B/Program.cs
+++ Grunnleggende OO og C#/Oppgaver/323B/Program.cs
@@ -4,18 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Gyldig tips: \n" +
-                          " - H, U, B\n" +
-                          " - halvgardering: HU, HB, UB\n" +
-                          " - helgardering: HUB\n" +
-                          "Skriv inn dine 12 tippinger med komma mellom hver (en tipping for hver kamp): ? ");
+            Console.Write("Gyldig tips: \n" + " - H, U, B\n" + " - halvgardering: HU, HB, UB\n" + " - helgardering: HUB\n" + "Skriv inn dine 12 tippinger med komma mellom hver (en tipping for hver kamp): ? ");
             var betsText = Console.ReadLine();
-            var bets = betsText.Split(',');
-            var matches = new Match[12];
-            for (var i = 0; i < 12; i++)
-            {
-                matches[i] = new Match(bets[i]);
-            }
+            var matches = new TwelveMatches(betsText);
 
             while (true)
             {
@@ -23,27 +14,12 @@
                 var command = Console.ReadLine();
                 if (command == "X") break;
                 var matchNo = Convert.ToInt32(command);
-                Console.Write("Kommandoer: \n" +
-                              " - H = scoring hjemmelag\n" +
-                              " - B = scoring bortelag\n" +
-                              " - X = kampen er ferdig\n" +
-                              "Angi kommando: ");
-                var team = Console.ReadLine();
-                var selectedIndex = matchNo - 1;
-                var selectedMatch = matches[selectedIndex];
-                selectedMatch.AddGoal(team == "H");
-                var correctCount = 0;
-                for (var index = 0; index < matches.Length; index++)
-                {
-                    var match = matches[index];
-                    var mathNo = index + 1;
-                    var isBetCorrect = match.IsBetCorrect();
-                    var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
-                    if (isBetCorrect) correctCount++;
-                    Console.WriteLine($"Kamp {matchNo}: {match.GetScore()} - {isBetCorrectText}");
-                }
+                Console.Write("Kommandoer: \n" + " - H = scoring hjemmelag\n" + " - B = scoring bortelag\n" + " - X = kampen er ferdig\n" + "Angi kommando: ");
 
-                Console.WriteLine($"Du har {correctCount} rette.");
+                var team = Console.ReadLine();
+                matches.AddGoal(matchNo, team == "H");
+                matches.ShowAllScores();
+                matches.ShowCorrectCount();
             }
         }
     }
