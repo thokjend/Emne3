994e433a7d1779b3cba01edf1830f83ffde6b490
thokjend <thokjend@online.no>
2024-05-27 14:23:20 +0200

    *

---
+++ Grunnleggende OO og C#/Oppgaver/329A/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _329A
+{
+    internal class Player
+    {
+        private string _name;
+        private int _points;
+        private readonly Random _random = new Random();
+
+        public Player(string name, int points)
+        {
+            _name = name;
+            _points = points;
+        }
+
+        public void Play(Player player2)
+        {
+            var drawRandomWinner = _random.Next(1);
+            player2._points += drawRandomWinner == 1 ? 1 : -1;
+            _points += drawRandomWinner == 0 ? 1 : -1;
+        }
+
+        public void ShowNameAndPoints()
+        {
+            Console.WriteLine($"{_name}: {_points} points");
+        }
+
+
+    }
+}
