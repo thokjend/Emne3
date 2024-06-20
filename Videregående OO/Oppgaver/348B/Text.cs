b68a2457cd9248ebe46b564f0b3be9c950432a84
thokjend <thokjend@online.no>
2024-06-20 12:09:10 +0200

    *

---
+++ Videregående OO/Oppgaver/348B/Text.cs
@@ -0,0 +1,20 @@
+﻿namespace _348B
+{
+    class Text : Shape
+    {
+        private string _text;
+        public Text(int x, int y, string text, Random random) : base(random)
+        {
+            X = x;
+            Y = y;
+            _text = text;
+        }
+
+        public override string GetCharacter(int row, int col)
+        {
+            if (row != Y || col < X || col >= X + _text.Length) return null;
+            var index = col - X;
+            return _text[index].ToString();
+        }
+    }
+}
